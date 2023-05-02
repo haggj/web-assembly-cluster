6223473b90519eccaeeb741701854dfff05fffd7
Jonas Hagg <jonas.hagg@tum.de>
2023-05-02 15:59:28 +0000

    react run wasm

--- cluster-frontend/src/pages/client/client.jsx
+++ cluster-frontend/src/pages/client/client.jsx
@@ -1,7 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import './wasm_exec.js';
 
 export const Client = () => {
     const [test, setTest] = useState('Client');
+
+    let initWebAssembly = async (wasm_path) => {
+       let go = new window.Go();
+       let result = await WebAssembly.instantiateStreaming(fetch(wasm_path, {cache: "no-store"}), go.importObject);
+       go.run(result.instance);
+     };
+
+    let runWebAssembly = (...args) => {
+        return window.wasmFunction(...args);
+    }
+
+   (async() => {
+  console.log('before start');
+  await initWebAssembly("http://localhost:3001/main.wasm");
+  const hash = Uint8Array.from([58, 196, 44, 131, 94, 101, 124, 169, 86, 7, 42, 215, 197, 160, 67, 233, 74, 171, 0, 112, 140, 38, 14, 99, 14, 16, 147, 84, 41, 159, 55, 51])
+  var result = runWebAssembly(hash, "hallo", "hi", "ho")
+  console.log(result);
+})();
+
 
     return (
         <h3>Page {test}</h3>
--- hpc-server/src/app.controller.ts
+++ hpc-server/src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, StreamableFile, Header } from '@nestjs/common';
 import { AppService } from './app.service';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
@@ -13,5 +15,13 @@ export class AppController {
   @Get('/jobs')
   getJobs(): string[] {
     return ['Hash-Cracker', 'Job #2', 'Prime calculator', '...']
+  }
+
+  @Get('/main.wasm')
+  @Header('Content-type', 'application/wasm')
+  getWasm(): StreamableFile {
+    let path = join(process.cwd(), 'main.wasm');
+    const file = createReadStream(path);
+    return new StreamableFile(file);
   }
 }
