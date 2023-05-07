626e8637cbe2bb52d2681c9f66156e8399190d0b
Jonas Hagg <jonas.hagg@tum.de>
2023-05-07 23:02:08 +0000

    client stats

--- cluster-frontend/src/pages/client/client.jsx
+++ cluster-frontend/src/pages/client/client.jsx
@@ -3,44 +3,56 @@ import './wasm_exec.js';
 import {io} from 'socket.io-client';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Accordion from 'react-bootstrap/Accordion';
 import {Link} from "react-router-dom";
+import Table from 'react-bootstrap/Table';
+var platform = require('platform');
 
 
 export const Client = () => {
     // Status information about the client for UI
-    const [loadedWasm, setLoadedWasm] = useState('');
-    const [jobName, setJobName] = useState('');
-    const [jobIsRunning, setJobIsRunning] = useState(false);
-    let [finishedJobs, setFinishedJobs] = useState(0);
+    let [loadedWasm, setLoadedWasm] = useState('');
 
     // Variables to maintain the websocket connection to the server
     let socket = null;
-    const [isConnected, setIsConnected] = useState(false)
+    let [isConnected, setIsConnected] = useState(false)
 
     // Stores the webworker which runs go wasm files
     let worker = null;
-    let job_start = null;
-    let latest_job = null;
+    let latestJobStart = null;
+    let latestJob = null;
+    let [latestJobName, setLatestJobName] = useState('');
+    let [jobIsRunning, setJobIsRunning] = useState(false);
+    let [finishedJobs, setFinishedJobs] = useState([]);
+
+    // Client detection
+    let [os, setOs] = useState(null);
+    let [browser, setBrowser] = useState(null);
 
 
-    const workerEventListener =  function(event) {
 
-        const { eventType, eventData, eventId } = event.data;
+    const workerEventListener = function (event) {
+
+        const {eventType, eventData, eventId} = event.data;
 
         if (eventType === "INITIALISED") {
             // Webworker is initialized, inform server that client is ready
-            setLoadedWasm(eventData);
+            loadedWasm = eventData;
+            setLoadedWasm(loadedWasm);
             console.log(eventData + " file loaded")
             // Send empty object to indicate that the WASM file was loaded and client is ready
             socket.emit('resultwasm', {});
         } else if (eventType === "RESULT") {
-            const job_end = Date.now();
+            // Webworker finished job
+            const latestJobEnd = Date.now();
+            latestJob.duration = latestJobEnd - latestJobStart;
+            latestJob.type = loadedWasm;
 
             setJobIsRunning(false);
-            finishedJobs += 1;
+            finishedJobs.push(latestJob);
             setFinishedJobs(finishedJobs);
 
-            let result = {id: latest_job.id, result: eventData, duration: job_end - job_start};
+            let result = {id: latestJob.id, result: eventData, duration: latestJob.duration};
             console.log("Result of job:")
             console.log(result)
             socket.emit('resultwasm', result);
@@ -68,11 +80,11 @@ export const Client = () => {
         console.log("Received job:")
         console.log(job)
 
-        setJobName(job.id.slice(0,10));
+        setLatestJobName(job.id.slice(0, 10));
         setJobIsRunning(true);
-        latest_job = job;
+        latestJob = job;
 
-        job_start = Date.now();
+        latestJobStart = Date.now();
         worker.postMessage({eventType: "CALL", eventData: job.data});
     }
 
@@ -84,7 +96,7 @@ export const Client = () => {
             setIsConnected(false)
         });
         sock.on("connect", (err) => {
-            if(socket != null){
+            if (socket != null) {
                 console.log("Disconnected earlier connection")
                 socket.disconnect()
             }
@@ -98,19 +110,30 @@ export const Client = () => {
 
 
     useEffect(() => {
+        setOs(platform.os)
+        setBrowser(platform.name)
         if (socket === null) {
             openWebSocket();
         }
     }, []);
 
 
+    const monospace = {
+        fontFamily: 'monospace',
+        backgroundColor: '#eee',
+        borderRadius: '5px',
+        display: 'inline-block',
+        padding: '8px'
+
+    }
+
     return (
         <div style={{margin: '30px'}}>
 
-            <Card style={{maxWidth: '700px'}}>
+            <Card style={{maxWidth: '800px'}}>
                 <Card.Body>
 
-                    <Card.Title>HPC Client</Card.Title>
+                    <Card.Title><h2>HPC Client</h2></Card.Title>
                     <Card.Subtitle>Your device is part of the WebAssembly cluster and is ready to execute
                         jobs.</Card.Subtitle>
 
@@ -119,11 +142,16 @@ export const Client = () => {
                             Status of your client
                         </ListGroup.Item>
                         <ListGroup.Item>
+                            Client detection:&emsp;&emsp;&emsp;
+                            <span style={monospace}>{browser + " @ " + os}</span>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Connection status:&emsp;&emsp;
                             {isConnected ?
                                 <>
+                                    Online
                                     <img src={require('./connected.gif')} width="40" height="40"
-                                         style={{marginRight: '10px'}}/>
-                                    Connected to server.
+                                         style={{marginLeft: '0px'}}/>
                                 </>
                                 :
                                 <>
@@ -132,11 +160,13 @@ export const Client = () => {
                             }
                         </ListGroup.Item>
                         <ListGroup.Item>
+                            {"Job status  :   "}&emsp;&emsp;&emsp;&emsp;&emsp;
                             {jobIsRunning ?
                                 <>
                                     <img src={require('./computing.gif')} width="35" height="35"
                                          style={{marginRight: '15px'}}/>
-                                    Job running: {loadedWasm} - {jobName}
+                                    Running {loadedWasm}{" - "}
+                                    <span style={monospace}> {latestJobName} </span>
                                 </>
                                 :
                                 <>
@@ -146,10 +176,62 @@ export const Client = () => {
 
                         </ListGroup.Item>
 
-                        <ListGroup.Item>Finished jobs: {finishedJobs} </ListGroup.Item>
+
+                        <ListGroup.Item style={{padding: "0px"}}>
+                            <Accordion flush>
+                                <Accordion.Item eventKey="0">
+                                    <Accordion.Header>
+                                        Finished jobs:&emsp;&emsp;&emsp;&emsp;
+                                        {finishedJobs.length}
+                                    </Accordion.Header>
+                                    <Accordion.Body>
+                                        {finishedJobs.length == 0 ?
+                                            <>
+                                                Your client has no finished jobs yet.
+                                            </>
+                                            :
+                                            <>
+                                                <span>Your client has finished the following jobs.</span>
+                                                <p> Average duration: {
+                                                    Math.round(finishedJobs.reduce((total, obj) => {
+                                                        return total + obj.duration;
+                                                    }, 0) / finishedJobs.length)
+                                                } ms</p>
+                                                <Table bordered hover>
+                                                    <thead>
+                                                    <tr>
+                                                        <th>#</th>
+                                                        <th>Type</th>
+                                                        <th>ID</th>
+                                                        <th>Duration</th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                    {finishedJobs.map((e, idx) => (
+                                                        <tr>
+                                                            <td>{idx + 1}</td>
+                                                            <td>{e.type}</td>
+                                                            <td><span style={monospace}>{e.id.slice(0, 10)}</span></td>
+                                                            <td>{e.duration} ms</td>
+                                                        </tr>
+                                                    ))}
+                                                    </tbody>
+                                                </Table>
+                                            </>
+
+                                        }
+
+                                    </Accordion.Body>
+                                </Accordion.Item>
+
+                            </Accordion>
+
+
+                        </ListGroup.Item>
                     </ListGroup>
                 </Card.Body>
             </Card>
+
 
             <Link to="/">Home</Link>
 
